a5df06751bdb26500d7fb4b4285f60c94b75da5d
Philip <creativepyt@gmail.com>
2020-04-29 00:12:30 +0200

    generating keys and startup script

--- setup_environment.py
+++ setup_environment.py
@@ -5,18 +5,35 @@ from cryptography.hazmat.backends import default_backend as crypto_default_backe
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
+TEMPLATE = """
+#!/bin/bash
+export SECRET_KEY='{key}'
+export SECRET_KEY_PRIVATE=`cat {keypath}/id_rsa`
+export SECRET_KEY_PUBLIC=`cat {keypath}/id_rsa.pub`
+export ALLOWED_HOSTS='["localhost"]'
+"""
 
-def gen_rsa(directory):
-    key = rsa.generate_private_key(backend=crypto_default_backend(), public_exponent=65537, key_size=2048)
-    private_key = key.private_bytes(crypto_serialization.Encoding.PEM, crypto_serialization.PrivateFormat.PKCS8, crypto_serialization.NoEncryption())
-    public_key = key.public_key().public_bytes(crypto_serialization.Encoding.OpenSSH, crypto_serialization.PublicFormat.OpenSSH)
 
-    if directory is not None:
-        with open(os.path.join(directory, 'id_rsa'), 'wb') as private:
-            private.write(private_key)
+def gen_ssh(dir):
+    key = rsa.generate_private_key(
+        backend=crypto_default_backend(),
+        public_exponent=65537,
+        key_size=2048
+    )
+    private_key = key.private_bytes(
+        crypto_serialization.Encoding.PEM,
+        crypto_serialization.PrivateFormat.PKCS8,
+        crypto_serialization.NoEncryption())
+    public_key = key.public_key().public_bytes(
+        crypto_serialization.Encoding.OpenSSH,
+        crypto_serialization.PublicFormat.OpenSSH
+    )
 
-        with open(os.path.join(directory, 'id_rsa.pub'), 'wb') as public:
-            public.write(public_key)
+    with open(os.path.join(dir, 'id_rsa'), 'wb') as private:
+        private.write(private_key)
+
+    with open(os.path.join(dir, 'id_rsa.pub'), 'wb') as public:
+        public.write(public_key)
 
 
 def gen_key():
@@ -24,14 +41,12 @@ def gen_key():
 
 
 def generate():
-    print("Generating secrets")
-    print("Generating RSA keys")
-    key_directory = os.path.join(os.getcwd(), "keys")
-    if not os.path.exists(key_directory):
-        os.mkdir(key_directory)
-    gen_rsa(key_directory)
-
-    print("Done")
+    with open("secrets2.sh", "w") as file:
+        directory = os.path.join(os.getcwd(), "keys")
+        if not os.path.exists(directory):
+            os.mkdir(directory)
+        gen_ssh(directory)
+        file.write(TEMPLATE.format(key=gen_key(), dbname="dbname", dbuser="dbuser", dbpass="dbpass", keypath=directory))
 
 
 if __name__ == '__main__':
