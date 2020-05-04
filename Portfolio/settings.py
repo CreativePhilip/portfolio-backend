e4485c43ee18cf8608cf032bf07f53f6f98870b0
Philip <creativepyt@gmail.com>
2020-05-04 14:43:36 +0200

    travis build file and added secret key as env variable

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1kh(x(kdc2-^k=r)jjwn&#jfc0^@fj+*81jx_m+uopocs52*uu'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
