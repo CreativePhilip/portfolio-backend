e20622c3336e472d7d3978dabd943fe5ebf0c324
philip <creativepyt@gmail.com>
2020-10-16 22:31:21 +0200

    travis env fix v3

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -7,5 +7,4 @@ from Portfolio import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('auth/', include('auth_app.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
